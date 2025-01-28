94afc0be134a72acef5c7d6da3d5094ecbfd19a9
Eto <sashapascaud06@gmail.com>
2025-01-28 14:11:58 +0100

    Update Pilesglobal.c

--- Calculatrice-inf-postfix/Pilesglobal.c
+++ Calculatrice-inf-postfix/Pilesglobal.c
@@ -1,110 +1,72 @@
-// pile.h
-#ifndef PILE_H
-#define PILE_H
+//queue.h
+#ifndef QUEUE_H
+#define QUEUE_H
 
-#include <stdbool.h>
-
-// Definition d'une structure pour une pile
-typedef struct Pile {
-    int *elements; // tableau dynamique pour stocker les elements
-    int capacite;  // taille maximale de la pile
-    int sommet;    // indice du sommet actuel de la pile
-} Pile;
-
-// Prototypes des fonctions
-Pile *creer_pile(int capacite);
-void detruire_pile(Pile *pile);
-bool est_vide(Pile *pile);
-bool est_pleine(Pile *pile);
-void empiler(Pile *pile, int valeur);
-void depiler(Pile *pile, int *valeur);
-int sommet_pile(Pile *pile);
-
-#endif // PILE_H
-
-// pile.c
-#include "pile.h"
 #include <stdlib.h>
-#include <stdio.h>
+#include <stdbool.h>
+#include <assert.h>
 
-Pile *creer_pile(int capacite) {
-    Pile *pile = (Pile *)malloc(sizeof(Pile));
-    if (!pile) {
-        fprintf(stderr, "Erreur : allocation de memoire pour la pile\n");
-        return NULL;
-    }
-    pile->elements = (int *)malloc(capacite * sizeof(int));
-    if (!pile->elements) {
-        fprintf(stderr, "Erreur : allocation de memoire pour les elements de la pile\n");
-        free(pile);
-        return NULL;
-    }
-    pile->capacite = capacite;
-    pile->sommet = -1;
-    return pile;
+typedef struct queue_s queue_t;
+
+queue_t *init(void);
+bool is_empty(queue_t *s);
+int top(queue_t *s);
+void push(int x, queue_t *s);
+void pop(queue_t *s);
+
+#endif
+
+
+
+
+//queue.c
+
+
+#include "queue.h"
+
+struct queue_s {
+  int *tab;
+  int taille;
+  int tete;
+  int max;
+};
+
+queue_t *init(void){
+  queue_t *pile = malloc(sizeof(queue_t));
+  (*pile).tab = malloc(20*sizeof(int));
+  (*pile).taille =  0;
+  (*pile).tete = 0;
+  (*pile).max = 20;
+  return pile;
 }
 
-void detruire_pile(Pile *pile) {
-    if (pile) {
-        free(pile->elements);
-        free(pile);
-    }
+bool is_empty(queue_t *queue){
+  return (queue->taille == 0);
 }
 
-bool est_vide(Pile *pile) {
-    return pile->sommet == -1;
+int top(queue_t *queue){
+  assert(! is_empty(queue));
+  return queue->tab[queue->tete];
 }
 
-bool est_pleine(Pile *pile) {
-    return pile->sommet == pile->capacite - 1;
+void redimensionne(queue_t *queue){
+  int *new_tab = malloc(2*queue->max*sizeof(int));
+  for(int i ; i<queue->max ; i = i+1){
+    new_tab[i] = queue->tab[i];
+  }
+  free(queue->tab);
+  queue->max = queue->max *2;
+  queue->tab = new_tab;
 }
 
-void empiler(Pile *pile, int valeur) {
-    if (est_pleine(pile)) {
-        fprintf(stderr, "Erreur : la pile est pleine\n");
-        return;
-    }
-    pile->elements[++pile->sommet] = valeur;
+void push(int elt, queue_t *queue){
+  if (queue->taille == queue->max){redimensionne(queue);}
+  queue->tab[queue->taille] = elt;
+  queue->taille++;
+  
 }
 
-void depiler(Pile *pile, int *valeur) {
-    if (est_vide(pile)) {
-        fprintf(stderr, "Erreur : la pile est vide\n");
-        return;
-    }
-    *valeur = pile->elements[pile->sommet--];
-}
-
-int sommet_pile(Pile *pile) {
-    if (est_vide(pile)) {
-        fprintf(stderr, "Erreur : la pile est vide\n");
-        return -1; // Valeur d'erreur
-    }
-    return pile->elements[pile->sommet];
-}
-
-// Exemple d'utilisation
-#include <stdio.h>
-
-int main() {
-    Pile *ma_pile = creer_pile(5);
-
-    empiler(ma_pile, 10);
-    empiler(ma_pile, 20);
-    empiler(ma_pile, 30);
-
-    printf("Sommet de la pile: %d\n", sommet_pile(ma_pile));
-
-    int valeur;
-    depiler(ma_pile, &valeur);
-    printf("Depile: %d\n", valeur);
-
-    depiler(ma_pile, &valeur);
-    printf("Depile: %d\n", valeur);
-
-    printf("Pile vide: %s\n", est_vide(ma_pile) ? "oui" : "non");
-
-    detruire_pile(ma_pile);
-
-    return 0;
+void pop(queue_t *queue){
+  assert(! is_empty(queue));
+  queue->taille--;
 }
